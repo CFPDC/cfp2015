70f1ee4b2e651b919fee16acef421914e134404c
Melani Rodriguez <Melani.Rodriguez@cgifederal.com>
2015-08-14 09:25:30 -0500

    Create reviewers and sponsors pages

---
+++ cfp-reviewers.php
@@ -0,0 +1,310 @@
+<?php
+include('includes/functions.php');
+?>
+<?php get_template('header', 'Title of homepage'); ?>
+<div class="container-fluid">
+	<div class="row">
+		<h1 class="text-center">Our Reviewers</h1>
+	</div>
+</div>
+<div class="clearfix">
+	<img src="http://lorempixel.com/1200/400/sports" style="width:100%" alt="First slide">
+</div>
+<main class="container-fluid search-results secondary" role="main">
+<div class="row">
+	<aside class="col-xs-12 col-sm-3 who-we-are-nav" id="asideFilter">
+		<ul style="list-style: none;">
+			<li><a href="about-us.php">About Us</a></li>
+			<li><a href="cfp-team.php">Team</a></li>
+			<li><a href="cfp-reviewers.php">Our Reviewers</a></li>
+			<li><a href="sponsors.php">Sponsors</a></li>
+			<li><a href="press.php">Press</a></li>
+			<li><a href="testimonials.php">Testimonials</a></li>
+			<li><a href="vision-report.php">Vision Report</a></li>
+		</ul>
+	</aside>
+	<section class="col-xs-12 col-sm-8">
+		<div class="row">
+			<div class="col-sm-12">
+			<p>Over one hundred reviewers from foundations, corporate giving programs, local government agencies, giving circles, peer nonprofits, and the philanthropic advisory community evaluate applications (about 225 a year) for distinction, merit, and impact. Approximately seventy nonprofits are selected annually. Every featured charity has been “site visited” by at least one reviewer, some by as many as five. Our accounting firm, RAFFA, PC, reviews the 990 filings, audited financials, and other key financial indicators of all finalists to ensure financial viability and transparency. And all organizations on the Catalogue website are re-certified every four years. We leverage the region’s knowledge about philanthropy for the benefit of the entire community.</p>
+			<p>The following participated in the selection process in 2014-15:</p>
+			<ul class="list-unstyled">
+				<li>
+					ACCESS Hope (Patricia Lunt)
+				</li>
+				<li>
+					ACT for Alexandria (John Porter)
+				</li>
+				<li>
+					America's Charities (Lynne Filderman)
+				</li>
+				<li>
+					Anacostia Watershed Society (Maureen Farrington &amp; Jim Foster)
+				</li>
+				<li>
+					AppleTree Institute (Jack McCarthy)
+				</li>
+				<li>
+					A Wider Circle (Mark Bergel, Pam Feinstein)
+				</li>
+				<li>
+					Ayuda (Jaime Farrant)
+				</li>
+				<li>
+					Baker Tilly (Raina Rose Tagle)
+				</li>
+				<li>
+					Black Benefactors (Celena Green, Maya Garrett Loggins, Tracey Webb)
+				</li>
+				<li>
+					Booz Allen Hamilton (James Murphy, Joseph T N Suarez)
+				</li>
+				<li>
+					Bushway Collins Strategies (Jane Collins)
+				</li>
+				<li>
+					Calvary Women's Services (Kristine Thompson)
+				</li>
+				<li>
+					The Case Foundation (Jade Floyd, Sarah Koch)
+				</li>
+				<li>
+					Center for Public and Nonprofit Leadership (Kathy P Kretman)
+				</li>
+				<li>
+					The Community Foundation for Prince George's County (Mena Amin, Amina Anderson)
+				</li>
+				<li>
+					Cornerstone (Nancy Liebermann)
+				</li>
+				<li>
+					Council on Foundations (Allison Carney)
+				</li>
+				<li>
+					Critical Exposure (Adam Levner)
+				</li>
+				<li>
+					DC Arts and Humanities Education Collaborative (Louise Kennelly)
+				</li>
+				<li>
+					DC Campaign to Prevent Teen Pregnancy (Brenda Rhodes Miller)
+				</li>
+				<li>
+					DC SCORES (Daniel Meloy)
+				</li>
+				<li>
+					Deloitte (Peter Cutler)
+				</li>
+				<li>
+					Donor Strategies (Barbara L Ciconte)
+				</li>
+				<li>
+					Empowered Women International (Dianne Lorenz)
+				</li>
+				<li>
+					Eugene and Agnes E Meyer Foundation (Maegan Scott, Jane Ward)
+				</li>
+				<li>
+					Fairfax County Office of Public Private Partnerships (Theresa Benincasa)
+				</li>
+				<li>
+					Fairfax Court Appointed Special Advocates (Lisa Banks)
+				</li>
+				<li>
+					The Fehlig Group (Mary Fehlig)
+				</li>
+				<li>
+					The Foundation Center (Patricia Pasqual)
+				</li>
+				<li>
+					Freddie Mac Foundation (Lori Vacek)
+				</li>
+				<li>
+					Georgetown Center for Social Impact Communication (Julie Dixon)
+				</li>
+				<li>
+					Giving Circle of HOPE (Linda Strup)
+				</li>
+				<li>
+					Harbour Capital Advisors (Betsy Duff)
+				</li>
+				<li>
+					Healthy Alexandria Giving Circle (Lynn O'Connell)
+				</li>
+				<li>
+					Homestretch (Christopher Fay)
+				</li>
+				<li>
+					The House, Student Leadership Center (Todd McCormick)
+				</li>
+				<li>
+					Howard &amp; Geraldine Polinger Family Foundation (Erica Pressman)
+				</li>
+				<li>
+					Independent Consultants (Kathy DeBoe, Anne Eigeman, Michael Hollins, Suzanne Martin, Ann Matikan, Sally Rudney, Margaret Siegel, Jonelle Stachura Wallmeyer)
+				</li>
+				<li>
+					Individuals (Sylvia Kidder Barry, Laura Choi, Jeffrey Fergus, Beth Hess, Sandra Hoehne, Lisa Kays, Laura Kelley, Robert Siciliano)
+				</li>
+				<li>
+					Independent Sector (Marie LeBlanc)
+				</li>
+				<li>
+					Integrity Management Consulting (Tracey Wright)
+				</li>
+				<li>
+					Intentional Philanthropy (Holli Rivera)
+				</li>
+				<li>
+					Iona Senior Services (Sally S White)
+				</li>
+				<li>
+					J Willard and Alice S Marriott Foundation (Meredith Asbury, Margaret Buckley)
+				</li>
+				<li>
+					Jack Kent Cooke Foundation (Natalie Jansorn, Astrik Tenney)
+				</li>
+				<li>
+					The JBG Companies (Bailey Hopple)
+				</li>
+				<li>
+					Jovid Foundation (Bob Wittig)
+				</li>
+				<li>
+					Jump Start Growth (Paul Jolly)
+				</li>
+				<li>
+					Junior League of Washington (Laura Blackwelder)
+				</li>
+				<li>
+					Laura Forman Communications (Laura Forman)
+				</li>
+				<li>
+					Little Lights Urban Ministries (Steve Park)
+				</li>
+				<li>
+					Local Initiatives Support Corporation (Oramenta Newsome)
+				</li>
+				<li>
+					Lois &amp; Richard England Family Foundation (Julia Baer-Cooper)
+				</li>
+				<li>
+					Luther I Replogle Foundation (Gwenn Gebhard)
+				</li>
+				<li>
+					Many Hands (Sydney Bath, Regina Hall, Sandra McCoy, Jill Meyer)
+				</li>
+				<li>
+					Miriam's Kitchen (Sara Gibson)
+				</li>
+				<li>
+					Montgomery County Office of Community Partnerships (Bruce Adams)
+				</li>
+				<li>
+					The Morris and Gwendolyn Cafritz Foundation (Michael Bigley, Karen Kinney, Debbi Lindenberg, Mary Mulcahy)
+				</li>
+				<li>
+					Naomi and Nehemiah Cohen Foundation (Alison McWilliams)
+				</li>
+				<li>
+					National Democratic Institute (Steve Whisnant)
+				</li>
+				<li>
+					The Nature Conservancy (Wendy Mills)
+				</li>
+				<li>
+					The NEA Foundation (Meg Porta)
+				</li>
+				<li>
+					Nonprofit Roundtable (Jon Squicciarini)
+				</li>
+				<li>
+					One World Education (Eric Goldstein)
+				</li>
+				<li>
+					Otto-Whalley Family Foundation (Judy Whalley)
+				</li>
+				<li>
+					Philip L Graham Fund (Caitlin Orth)
+				</li>
+				<li>
+					PortSmith Creations (Donna Purchase)
+				</li>
+				<li>
+					Potomac Conservancy (Hedrick Belin)
+				</li>
+				<li>
+					Raffa (Jimena Ryan)
+				</li>
+				<li>
+					Reach for College! (Jonathan Williams)
+				</li>
+				<li>
+					Reach Incorporated (Mark Hecker)
+				</li>
+				<li>
+					Reading Partners (Lisa Lazarus)
+				</li>
+				<li>
+					Richard E and Nancy P Marriott Foundation (Nancie Suzuki)
+				</li>
+				<li>
+					Rosso Writing (Betsy Rosenblatt Rosso)
+				</li>
+				<li>
+					The Senior Connection of Montgomery County (Sue Dollins)
+				</li>
+				<li>
+					Share Our Strength (Suzy Twohig)
+				</li>
+				<li>
+					Sidgmore Family Foundation (Marian Gelbwaks)
+				</li>
+				<li>
+					Sitar Arts Center (Monica Harrington)
+				</li>
+				<li>
+					Sterling Martin Associates (Virginia Record)
+				</li>
+				<li>
+					Trawick Foundation (Carol Trawick)
+				</li>
+				<li>
+					TTR Sotheby's International Realty (Jennifer Hammond)
+				</li>
+				<li>
+					United Way of the National Capital Area (Levina Kim)
+				</li>
+				<li>
+					US Trust (Lis Driscoll)
+				</li>
+				<li>
+					Volunteer Fairfax (Susan Sanow)
+				</li>
+				<li>
+					Washington Jesuit Academy (Suzanne Armstrong)
+				</li>
+				<li>
+					Washington Performing Arts Society (Doug Wheeler)
+				</li>
+				<li>
+					Young Playwrights' Theater (Brigitte Pribnow Moore)
+				</li>
+			</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="well">
+					Financial Review by Raffa, PC: Tom Raffa along with Stacy Wu and Nicole De Vera; Desislava Cekova, Allison Griesbach, Andrea Haywood, Rachael Hess, Wade Simmons, Cuong Vuong
+				</div>
+			</div>
+			<div class="col-sm-12">
+				<div class="well">
+					 Special thanks to: Cynthia Evans, Julie Jones, Simone Putnam; Lyndsey Scofield, Martise Gaymon; Mike Madden, Amy Austin; Leigh Vogel; Lynne Filderman; Bill Cain; Special thanks to Sherika Brooks, Stacy Sharpe, and departing Chair Don Neal.
+				</div>
+			</div>
+		</div>
+	</section>
+	</main>
+<?php get_template('footer'); ?>
